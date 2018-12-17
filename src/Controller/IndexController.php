bbc32416ea9442263f68a83ccd211ad8ecb879d3
Jens Vervaeren <vervaeren.jens@gmail.com>
2018-12-17 19:02:17 +0100

    * Added PHP templating package

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IndexController
+class IndexController extends AbstractController
 {
 	/** 
 	 * @Route("/")
 	 */
 	public function index()
 	{
-		return new Response('<html><body>This is a test</body></html>');
+		return $this->render('index/index.html.php', []);
 	}
 }
---
+++ src/Resources/views/index/index.html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AutoManager ToDo-List</title>
+	<?php $view->extend('js.html.php') ?>
+	<?php $view->extend('css.html.php') ?>
+</head>
+<body>
+	
+</body>
+</html>
