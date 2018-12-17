1d72245cc23ecf8c0660291568695f28199db3ee
Jens Vervaeren <vervaeren.jens@gmail.com>
2018-12-17 16:29:59 +0100

    * Setup of IndexController * Added annotations package

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController
+{
+	/** 
+	 * @Route("/")
+	 */
+	public function index()
+	{
+		return new Response('<html><body>This is a test</body></html>');
+	}
+}
